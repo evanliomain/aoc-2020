462519064bade9a61b96eca4038ea58be45bb293
LIOMAIN Evan <liomain.e@paris.sfeir.com>
2020-12-11 09:36:47 +0100

    ♻️ Refacto day 11, exercice 1

--- days/11/level1.js
+++ days/11/level1.js
@@ -1,28 +1,47 @@
 const T = require('taninsam');
-const { printLayout, nextGeneration, countOccupiedSeats } = require('./tools');
+const { findEquilibre } = require('./tools');
+const { atMatrix } = require('../../tools');
 
 module.exports = function(input) {
   return T.chain(input)
-    .chain(matrix => ({
-      matrix,
-      previous: null,
-      next: countOccupiedSeats(matrix)
-    }))
-    .chain(
-      T.loopWhile(
-        ({ previous, next }) => previous !== next,
-        ({ matrix, next }) =>
-          T.chain(matrix)
-            .chain(nextGeneration)
-            .chain(newMatrix => ({
-              matrix: newMatrix,
-              previous: next,
-              next: countOccupiedSeats(newMatrix)
-            }))
-            .value()
-      )
-    )
-    .chain(({ matrix }) => matrix)
-    .chain(countOccupiedSeats)
+    .chain(findEquilibre({ tolerantIndex: 4, countOccupiedSeatsAdjacent }))
     .value();
 };
+
+function countOccupiedSeatsAdjacent(matrix) {
+  const counter = cell =>
+    T.chain(cell)
+      .chain(isOccupiedSeats(matrix))
+      .chain(boolToInt)
+      .value();
+  return ({ x, y }) =>
+    counter({ x: x - 1, y }) +
+    counter({ x: x + 1, y }) +
+    counter({ x, y: y - 1 }) +
+    counter({ x, y: y + 1 }) +
+    counter({ x: x - 1, y: y - 1 }) +
+    counter({ x: x + 1, y: y - 1 }) +
+    counter({ x: x - 1, y: y + 1 }) +
+    counter({ x: x + 1, y: y + 1 });
+}
+
+function isOccupiedSeats(matrix) {
+  return ({ x, y }) => {
+    if (-1 === y) {
+      return false;
+    }
+    if (-1 === x) {
+      return false;
+    }
+    if (matrix.length <= y) {
+      return false;
+    }
+    if (matrix[0].length <= x) {
+      return false;
+    }
+    return '#' === atMatrix({ x, y })(matrix);
+  };
+}
+function boolToInt(bool) {
+  return bool ? 1 : 0;
+}
--- days/11/tools.js
+++ days/11/tools.js
@@ -1,24 +1,56 @@
 const chalk = require('chalk');
 const T = require('taninsam');
-const {
-  printMatrix,
-  patternMatching,
-  mapMatrix,
-  atMatrix
-} = require('../../tools');
+const { printMatrix, patternMatching, mapMatrix } = require('../../tools');
 
-function nextGeneration(matrix) {
-  const counter = countOccupiedSeatsAdjacent(matrix);
-  return mapMatrix((cell, x, y) =>
-    rule({ cell, nbOccupiedSeatsAdjacent: counter({ x, y }), x, y })
-  )(matrix);
+function findEquilibre({ tolerantIndex, countOccupiedSeatsAdjacent }) {
+  return input =>
+    T.chain(input)
+      .chain(matrix => ({
+        matrix,
+        previous: null,
+        next: countOccupiedSeats(matrix)
+      }))
+      .chain(
+        T.loopWhile(
+          ({ previous, next }) => previous !== next,
+          ({ matrix, next }) =>
+            T.chain(matrix)
+              .chain(
+                nextGeneration({ tolerantIndex, countOccupiedSeatsAdjacent })
+              )
+              .chain(newMatrix => ({
+                matrix: newMatrix,
+                previous: next,
+                next: countOccupiedSeats(newMatrix)
+              }))
+              .value()
+        )
+      )
+      .chain(({ matrix }) => matrix)
+      .chain(countOccupiedSeats)
+      .value();
 }
 
-function rule({ cell, nbOccupiedSeatsAdjacent, x, y }) {
+function nextGeneration({ tolerantIndex, countOccupiedSeatsAdjacent }) {
+  return matrix => {
+    const counter = countOccupiedSeatsAdjacent(matrix);
+    return mapMatrix((cell, x, y) =>
+      rule({
+        tolerantIndex,
+        cell,
+        nbOccupiedSeatsAdjacent: counter({ x, y }),
+        x,
+        y
+      })
+    )(matrix);
+  };
+}
+
+function rule({ cell, nbOccupiedSeatsAdjacent, tolerantIndex }) {
   if ('L' === cell && 0 === nbOccupiedSeatsAdjacent) {
     return '#';
   }
-  if ('#' === cell && 4 <= nbOccupiedSeatsAdjacent) {
+  if ('#' === cell && tolerantIndex <= nbOccupiedSeatsAdjacent) {
     return 'L';
   }
   return cell;
@@ -39,46 +71,9 @@ const countOccupiedSeats = T.sumBy(
   )
 );
 
-function countOccupiedSeatsAdjacent(matrix) {
-  const counter = cell =>
-    T.chain(cell)
-      .chain(isOccupiedSeats(matrix))
-      .chain(boolToInt)
-      .value();
-  return ({ x, y }) =>
-    counter({ x: x - 1, y }) +
-    counter({ x: x + 1, y }) +
-    counter({ x, y: y - 1 }) +
-    counter({ x, y: y + 1 }) +
-    counter({ x: x - 1, y: y - 1 }) +
-    counter({ x: x + 1, y: y - 1 }) +
-    counter({ x: x - 1, y: y + 1 }) +
-    counter({ x: x + 1, y: y + 1 });
-}
-
-function isOccupiedSeats(matrix) {
-  return ({ x, y }) => {
-    if (-1 === y) {
-      return false;
-    }
-    if (-1 === x) {
-      return false;
-    }
-    if (matrix.length <= y) {
-      return false;
-    }
-    if (matrix[0].length <= x) {
-      return false;
-    }
-    return '#' === atMatrix({ x, y })(matrix);
-  };
-}
-function boolToInt(bool) {
-  return bool ? 1 : 0;
-}
-
 module.exports = {
   printLayout,
+  findEquilibre,
   countOccupiedSeats,
   nextGeneration
 };
