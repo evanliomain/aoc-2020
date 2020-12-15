d7e67ce99686994ced43d966086b1c60d260b044
LIOMAIN Evan <liomain.e@paris.sfeir.com>
2020-12-15 11:39:36 +0100

    ♻️ Last variable naming refacto for day 15

--- days/15/tools.js
+++ days/15/tools.js
@@ -4,17 +4,15 @@ module.exports = {
 
 function lastSpokenNumber(nbIterations) {
   return input => {
-    const lastSeen = new Map();
+    const seen = new Map();
 
-    input.forEach((n, i) => {
-      lastSeen.set(n, i + 1);
-    });
+    input.forEach((n, i) => seen.set(n, i + 1));
+
     let lastSpoken = input[input.length - 1];
+
     for (let turn = input.length; turn < nbIterations; turn++) {
-      const turnLastSeen = lastSeen.has(lastSpoken)
-        ? lastSeen.get(lastSpoken)
-        : turn;
-      lastSeen.set(lastSpoken, turn);
+      const turnLastSeen = seen.has(lastSpoken) ? seen.get(lastSpoken) : turn;
+      seen.set(lastSpoken, turn);
       lastSpoken = turn - turnLastSeen;
     }
     return lastSpoken;
