4737eef66e847e34d1e0764be5f9ad73fdc6c0d7
Evan Liomain <evan.liomain@gmail.com>
2020-12-19 08:04:02 +0100

    ♻️ Refacto day 19, exercice 1

--- days/19/in.js
+++ days/19/in.js
@@ -1,6 +1,6 @@
 const T = require('taninsam');
 
-const { captureGroups, replace, matchRegexp } = require('../../tools');
+const { captureGroups } = require('../../tools');
 
 const extractRE = /^(?<name>\d+): (?<rule>.*)$/;
 
@@ -10,36 +10,11 @@ module.exports = function(input) {
     .chain(([rules, messages]) => ({ rules, messages }))
     .chain(({ messages, rules }) => ({
       messages,
-      rule: T.chain(rules)
+      rules: T.chain(rules)
         .chain(T.map(captureGroups(extractRE)))
         .chain(T.map(({ name, rule }) => [name, rule.replaceAll('"', '')]))
         .chain(T.fromEntries())
-        .chain(rulesToRegex)
-        .chain(rule => new RegExp(rule))
         .value()
     }))
     .value();
 };
-
-function rulesToRegex(rules) {
-  return T.chain(rules[0])
-    .chain(
-      T.loopWhile(
-        T.not(matchRegexp(/^[^\d]+$/)),
-        T.chainFn(T.split(' '))
-          .chain(
-            T.map(
-              T.unless(
-                T.not(matchRegexp(/^\d+$/)),
-                part => `( ${rules[part]} )`
-              )
-            )
-          )
-          .chain(T.join(' '))
-          .value()
-      )
-    )
-    .chain(replace(/ /g, ''))
-    .chain(rule => `^${rule}$`)
-    .value();
-}
--- days/19/level1.js
+++ days/19/level1.js
@@ -1,9 +1,10 @@
 const T = require('taninsam');
 const { matchRegexp } = require('../../tools');
+const { rulesToRegex } = require('./tools');
 
-module.exports = function({ messages, rule }) {
+module.exports = function({ messages, rules }) {
   return T.chain(messages)
-    .chain(T.filter(matchRegexp(rule)))
+    .chain(T.filter(matchRegexp(new RegExp(rulesToRegex(rules)))))
     .chain(T.length())
     .value();
 };
