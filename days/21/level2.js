0b1923849074e504bc1fa27f0f0a89b502aac9a3
Evan Liomain <evan.liomain@gmail.com>
2020-12-21 10:00:47 +0100

    ♻️ Refacto day 21, exercice 2

--- days/21/level2.js
+++ days/21/level2.js
@@ -2,31 +2,40 @@ const T = require('taninsam');
 const { getInertIngredients } = require('./tools');
 
 module.exports = function(foods) {
-  const inertIngredients = getInertIngredients(foods);
+  return T.chain(foods)
+    .chain(getIngredientsAllergensMapping)
+    .chain(T.sortBy(({ allergen }) => allergen))
+    .chain(T.map(({ ingredient }) => ingredient))
+    .chain(T.join(','))
+    .value();
+};
 
-  const allergenIngredients = foods.map(({ ingredients, allergens }) => ({
-    allergens,
-    ingredients: ingredients.filter(
-      ingredient => !inertIngredients.has(ingredient)
-    )
-  }));
+function getIngredientsAllergensMapping(foods) {
+  const inertIngredients = getInertIngredients(foods);
 
   const ingredientsMap = new Map();
 
-  allergenIngredients.forEach(({ allergens, ingredients }) => {
-    allergens.forEach(allergen => {
-      if (!ingredientsMap.has(allergen)) {
-        ingredientsMap.set(allergen, new Set(ingredients));
-      } else {
-        const possiblesAllergens = ingredientsMap.get(allergen);
-        possiblesAllergens.forEach(possibleAllergen => {
-          if (!ingredients.includes(possibleAllergen)) {
-            possiblesAllergens.delete(possibleAllergen);
-          }
-        });
-      }
+  foods
+    .map(({ ingredients, allergens }) => ({
+      allergens,
+      ingredients: ingredients.filter(
+        ingredient => !inertIngredients.has(ingredient)
+      )
+    }))
+    .forEach(({ allergens, ingredients }) => {
+      allergens.forEach(allergen => {
+        if (!ingredientsMap.has(allergen)) {
+          ingredientsMap.set(allergen, new Set(ingredients));
+        } else {
+          const possiblesAllergens = ingredientsMap.get(allergen);
+          possiblesAllergens.forEach(possibleAllergen => {
+            if (!ingredients.includes(possibleAllergen)) {
+              possiblesAllergens.delete(possibleAllergen);
+            }
+          });
+        }
+      });
     });
-  });
 
   const foundAllergens = new Set();
 
@@ -43,16 +52,11 @@ module.exports = function(foods) {
       }
     });
   }
-
-  return T.chain([...ingredientsMap])
-    .chain(
-      T.map(([key, values]) => ({ ingredient: [...values][0], allergen: key }))
-    )
-    .chain(T.sortBy(({ allergen }) => allergen))
-    .chain(T.map(({ ingredient }) => ingredient))
-    .chain(T.join(','))
-    .value();
-};
+  return [...ingredientsMap].map(([key, values]) => ({
+    ingredient: [...values][0],
+    allergen: key
+  }));
+}
 
 function hasFoundAssociation(ingredientsMap) {
   let result = true;
